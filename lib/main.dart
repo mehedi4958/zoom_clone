9cbef04e49f7557e6e49610c2658e9a097ce8017
Mehedi <mehedi@augnitive.com>
2022-11-06 22:25:59 +0600

    assets, custom color added for custom configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
 import 'package:zoom_clone/views/screens/login_screen.dart';
 
 void main() {
@@ -14,8 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
       ),
       home: const LoginScreen(),
     );
